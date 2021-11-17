c1e599c8a59c9209170bcd80b0038516da7165fe
officialozioma <dikep15@gmail.com>
2021-11-17 02:35:40 +0100

    database details update

--- config/database.php
+++ config/database.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Str;
 
+$DATABASE_URL = parse_url(getenv('DATABASE_URL'));
+
 return [
     /*
     |--------------------------------------------------------------------------
@@ -63,19 +65,33 @@ return [
                 : [],
         ],
 
+        // 'pgsql' => [
+        //     'driver' => 'pgsql',
+        //     'url' => env('DATABASE_URL'),
+        //     'host' => env('DB_HOST', '127.0.0.1'),
+        //     'port' => env('DB_PORT', '5432'),
+        //     'database' => env('DB_DATABASE', 'articlespace'),
+        //     'username' => env('DB_USERNAME', 'forgepostgres'),
+        //     'password' => env('DB_PASSWORD', 'root'),
+        //     'charset' => 'utf8',
+        //     'prefix' => '',
+        //     'prefix_indexes' => true,
+        //     'schema' => 'public',
+        //     'sslmode' => 'prefer',
+        // ],
+
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'articlespace'),
-            'username' => env('DB_USERNAME', 'forgepostgres'),
-            'password' => env('DB_PASSWORD', 'root'),
+            'host' => env('DB_HOST') ?? $DATABASE_URL['host'],
+            'port' => env('DB_PORT') ?? $DATABASE_URL['port'],
+            'database' =>
+                env('DB_DATABASE') ?? ltrim($DATABASE_URL['path'], '/'),
+            'username' => env('DB_USERNAME') ?? $DATABASE_URL['user'],
+            'password' => env('DB_PASSWORD') ?? $DATABASE_URL['pass'],
             'charset' => 'utf8',
             'prefix' => '',
-            'prefix_indexes' => true,
             'schema' => 'public',
-            'sslmode' => 'prefer',
+            'sslmode' => 'require',
         ],
 
         'sqlsrv' => [
